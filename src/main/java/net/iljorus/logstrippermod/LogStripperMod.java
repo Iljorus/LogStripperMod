64a3d419b40e9ee0fa4d05412ca1cc17bf157eb0
Iljorus <emilkrieger@mailbox.org>
2026-03-11 13:26:21 +0100

    Refactor mod registration setup

--- src/main/java/net/iljorus/logstrippermod/LogStripperMod.java
+++ src/main/java/net/iljorus/logstrippermod/LogStripperMod.java
@@ -7,14 +7,14 @@ import net.iljorus.logstrippermod.config.BaseConfig;
 import net.iljorus.logstrippermod.gui.LogStripperScreen;
 import net.iljorus.logstrippermod.gui.ModMenuTypes;
 import net.iljorus.logstrippermod.item.ModItems;
+import net.iljorus.logstrippermod.network.ModPacketHandler;
 import net.iljorus.logstrippermod.recipe.ModRecipes;
 import net.iljorus.logstrippermod.util.Utils;
 import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModContainer;
@@ -49,42 +49,33 @@ public class LogStripperMod {
 
         //Register Config
         context.registerConfig(ModConfig.Type.COMMON, BaseConfig.COMMON_SPEC);
-
-        modEventBus.addListener(this::commonSetup);
-
-        MinecraftForge.EVENT_BUS.register(this);
-
-        modEventBus.addListener(this::addCreative);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
-    }
-
-
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
-    }
-
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
+    public static class ClientEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
             MenuScreens.register(ModMenuTypes.LOG_STRIPPER_MENU.get(), LogStripperScreen::new);
         }
     }
 
-    @Mod.EventBusSubscriber(modid = MOD_ID)
-    public static class CoreEvents {
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+    public static class CoreForgeEvents {
         @SubscribeEvent
         public static void serverTick(TickEvent.ServerTickEvent event) {
             if (event.phase == TickEvent.Phase.START) {
                 Utils.tickTime();
             }
         }
+
+        @SubscribeEvent
+        public static void onServerStarted(ServerStartedEvent event) {
+
+        }
+
+        @SubscribeEvent
+        public static void loadEvent(LevelEvent.Load event) {
+        }
+    }
     }
 }
