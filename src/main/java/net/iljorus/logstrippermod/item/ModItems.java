999b8b21ad7e21ad712c000f840714c8df40c579
Iljorus <emilkrieger@mailbox.org>
2026-03-11 10:25:49 +0100

    Add first of many new item

--- src/main/java/net/iljorus/logstrippermod/item/ModItems.java
+++ src/main/java/net/iljorus/logstrippermod/item/ModItems.java
@@ -5,11 +5,14 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, LogStripperMod.MOD_ID);
 
+    public static final RegistryObject<Item> DIGITAL_AXE_HEAD = ITEMS.register("digital_axe_head",
+            () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
